eb1887689146c05d3bfb902c826d9cd2eb25d20a
dimkabuzz <dimkabuzz@gmail.com>
2022-03-11 20:01:37 +0100

    Sticky nav JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-"use strict";
+'use strict';
+// Sticky navigation
+const headerTop = document.querySelector('.header__top');
+const headerBottom = document.querySelector('.header__bottom');
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting)
+    headerBottom.classList.add('header__bottom--sticky');
+  else headerBottom.classList.remove('header__bottom--sticky');
+};
+
+const stickyNavObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `${headerBottom.getBoundingClientRect().height / 2}px`,
+});
+
+stickyNavObserver.observe(headerTop);
