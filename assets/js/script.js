609c440abba354caa978b68a0ac9cdf65a96e4e0
dimkabuzz <dimkabuzz@gmail.com>
2022-03-13 21:08:54 +0100

    Categories dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,20 @@
 'use strict';
-// Sticky navigation
+
 const headerTop = document.querySelector('.header__top');
 const headerBottom = document.querySelector('.header__bottom');
+const categoriesBtn = document.querySelector('#categories-button');
+const categoriesBtnMore = document.querySelector('.categories-dropdown__more');
+const categoriesBtnMoreIcon = document.querySelector(
+  '.categories-dropdown__more-icon'
+);
+const categoriesDropdown = document.querySelector('.categories-dropdown');
+const categoriesDropdownList = document.querySelector(
+  '.categories-dropdown__list'
+);
 
+// Sticky navigation
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting)
     headerBottom.classList.add('header__bottom--sticky');
@@ -15,7 +24,32 @@ const stickyNav = function (entries) {
 const stickyNavObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
-  rootMargin: `${headerBottom.getBoundingClientRect().height / 2}px`,
 });
 
 stickyNavObserver.observe(headerTop);
+
+// Show/hide categories dropdown
+const resetCategoriesDropDawn = function () {
+  categoriesDropdown.classList.add('hidden');
+  categoriesBtn.querySelector('.fi-rs-angle-up').classList = 'fi-rs-angle-down';
+  categoriesDropdownList.classList.remove('categories-dropdown__list--full');
+  categoriesBtnMoreIcon.classList.remove('open');
+};
+
+categoriesBtn.addEventListener('click', function (e) {
+  e.preventDefault;
+
+  if (categoriesDropdown.classList.contains('hidden')) {
+    categoriesDropdown.classList.remove('hidden');
+    categoriesBtn.querySelector('.fi-rs-angle-down').classList =
+      'fi-rs-angle-up';
+  } else {
+    resetCategoriesDropDawn();
+  }
+});
+
+// Show/hide MORE categories
+categoriesBtnMore.addEventListener('click', function () {
+  categoriesDropdownList.classList.toggle('categories-dropdown__list--full');
+  categoriesBtnMoreIcon.classList.toggle('open');
+});
